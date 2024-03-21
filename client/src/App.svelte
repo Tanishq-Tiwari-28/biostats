08739516a5994fa763fb91436a0fa4ebcb2515dd
vens8 <tanishq2149621496@iiitd.ac.in>
2024-03-21 12:41:16 +0530

    Initiial Commit

---
+++ client/src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+    let message = '';
+
+    fetch('http://localhost:5000/api/data')
+        .then(response => response.json())
+        .then(data => message = data.message);
+</script>
+
+<main>
+    <h1>{message}</h1>
+</main>
