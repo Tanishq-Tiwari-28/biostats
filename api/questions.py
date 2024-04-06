f842e198f1cc5def3bc491414471e081a9f08c4a
Tanishq-Tiwari-28 <tanishq21496@iiitd.ac.in>
2024-04-06 15:10:50 +0530

    DONE

--- api/questions.py
+++ api/questions.py
@@ -1,11 +1,8 @@
 questions = {
-    38:{
-
-    },
     0:{
         "question" : "DO YOU WANT TO KNOW BEST STATISTICAL TEST FOR YOUR DATA!! TELL US ABOUT YOUR DATA AND WE WILL HELP YOU OUT 😊",
         "options" : ["Tell us about your data, Upload the File."],
-        "next" : [38],
+        "next" : [1],
     },
     1: { # start
         "question": "IS THERE ONLY ONE VARIABLE OF INTEREST?🤔",
